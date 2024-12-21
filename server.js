6f1b82a56c384fd13be8f945634334b0a4dd89cf
Aldrin Lobis <aldrinlobi1@gmail.com>
2024-12-21 12:15:56 +0800

    fixing backend

--- server.js
+++ server.js
@@ -21,21 +21,22 @@ app.use(
     credentials: true,
   })
 );
-// app.get("/",async (req,res)=>{
-//   // try {
-//   //   const events=await Event.find();
-//   //   res.status(200).json(events);  
-//   // } catch (error) {
-//   //   res.status(500).json({message:"Server Error",error});
-//   // }
-//   // try {
-//   //   await mongoose.connection.db.command({ ping: 1 });
-//   //   res.status(200).send("MongoDB is connected!");
-//   // } catch (error) {
-//   //   res.status(500).json({message:"Mongo db not connected",error});
-//   //   // res.status(500).send("MongoDB is not connected.");
-//   // }
-// })
+app.get("/",async (req,res)=>{
+  // try {
+  //   const events=await Event.find();
+  //   res.status(200).json(events);  
+  // } catch (error) {
+  //   res.status(500).json({message:"Server Error",error});
+  // }
+  try {
+    await connectDB();
+    // await mongoose.connection.db.command({ ping: 1 });
+    res.status(200).send("MongoDB is connected!");
+  } catch (error) {
+    res.status(500).json({message:"Mongo db not connected",error});
+    // res.status(500).send("MongoDB is not connected.");
+  }
+})
 // app.use("/",eventRoutes);
 app.get("/api/events",getEvents)
 app.use("/api/auth/", authRoutes);
@@ -46,5 +47,5 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listing to port ${port}`);
-  connectDB();
+  // connectDB();
 });
